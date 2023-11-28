95876d4d6a961964aa16efd038134dabea6bc62d
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-11-28 01:20:13 +0100

    ultimo

---
+++ app/src/main/java/com/example/promul_trabajo_final_trimestre/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.promul_trabajo_final_trimestre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
